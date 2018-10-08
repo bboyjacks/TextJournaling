cb4edc4be68b7622c0e188f5eb11c8fb507d1371
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro.local>
2018-10-08 09:12:50 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+# Filename: app.py
+# Author: Lester Dela Cruz
+# Github: bboyjacks
+# Description: This app receives a message from phone
+# and records them in a database then shows them in 
+# a UI
