f22b407b392430ea58472cee27d9a11ec5e51fa7
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro.local>
2018-10-09 11:41:31 -0700

    updated app.py

--- app.py
+++ app.py
@@ -4,3 +4,12 @@
 # Description: This app receives a message from phone
 # and records them in a database then shows them in 
 # a UI
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
