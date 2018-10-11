55a835fab89d3caf26813341b7fc677e338f2700
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro.local>
2018-10-11 14:23:28 -0700

    working querying diary from sql

--- app.py
+++ app.py
@@ -12,8 +12,9 @@ mood_db = MoodDB()
 
 @app.route('/')
 def home():
-    moods = mood_db.get_moods()        
-    return render_template('home.html', moods=moods)
+    moods = mood_db.get_moods(1)
+    diaries = mood_db.get_diaries(1)
+    return render_template('home.html', moods=moods, diaries=diaries)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- mood_db.py
+++ mood_db.py
@@ -7,18 +7,18 @@ class MoodDB:
 		self.db_uri = 'sqlite:///mood_sms.db'
 		self.engine = create_engine(self.db_uri)
 	
-	def get_moods(self):
-		moods = self.engine.execute("SELECT type, star, time FROM users INNER JOIN mood ON users.id = mood.user_id AND users.id = 1")
+	def get_moods(self, user_id):
+		moods = self.engine.execute("SELECT type, star, time FROM users INNER JOIN mood ON users.id = mood.user_id AND users.id = " + str(user_id))
 		mood_list = {'moods': []}
 		for mood in moods:
 			mood_obj = {}
 			mood_obj['type'] = mood[0]
 			mood_obj['star'] = mood[1]
-			mood_obj['time'] = self.__time_parser__(mood[2])
+			mood_obj['time'] = self.__time_parser(mood[2])
 			mood_list['moods'].append(mood_obj)
 		return mood_list
 
-	def __time_parser__(self, mood_time):
+	def __time_parser(self, mood_time):
 		dt = datetime.datetime.utcfromtimestamp(mood_time)
 		dt_today = datetime.datetime.now()
 
@@ -26,3 +26,14 @@ class MoodDB:
 			return dt_today.strftime('%I %p Today')
 		else:
 			return dt.strftime('%I %p on %m/%d/%Y')
+	
+	def get_diaries(self, user_id):
+		diaries = self.engine.execute("SELECT post, time FROM users INNER JOIN diary ON users.id = diary.user_id AND users.id = " + str(user_id))
+		diary_list = {'diaries': []}
+		
+		for diary in diaries:
+			diary_obj = {}
+			diary_obj['post'] = diary[0]
+			diary_obj['time'] = self.__time_parser(diary[1])
+			diary_list['diaries'].append(diary_obj)
+		return diary_list
